1a5a9a43cb32ddc8c4fbbd8e042b78a9dad50bae
JetroPuranen <jetropuranenn@gmail.com>
2024-08-27 15:38:40 +0300

    Added entities and dbcontext

---
+++ PostIt.Domain/Entities/BaseEntity.cs
@@ -0,0 +1,19 @@
+﻿
+namespace PostIt.Domain.Entities
+{
+    
+    public class BaseEntity
+    {
+        public Guid Id { get; set; }
+
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+
+        public DateTime UpdatedAt { get; set; } = DateTime.Now;
+
+        public BaseEntity()
+        {
+            Id = Guid.NewGuid();
+        }
+    }
+
+}
