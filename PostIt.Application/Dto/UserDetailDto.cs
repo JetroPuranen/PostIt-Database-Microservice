004f014f4d9cd02bc1f1510c69c9efebf92faa0c
JetroPuranen <jetropuranenn@gmail.com>
2024-11-10 21:00:24 +0200

    Added follower return values and birthday return value

--- PostIt.Application/Dto/UserDetailDto.cs
+++ PostIt.Application/Dto/UserDetailDto.cs
@@ -6,6 +6,19 @@
         public string? Username { get; set; }
         public string? FirstName { get; set; }
         public string? SurName { get; set; }
+
+        public DateTime? BirhtDay { get; set; }
+        public List<SimpleUserDto> Followers { get; set; } = new();
+        public List<SimpleUserDto> Following { get; set; } = new();
+
         public byte[]? ProfilePicture { get; set; }
+        
+    }
+
+    public class SimpleUserDto
+    {
+        public Guid UserId { get; set; }
+        public string? Username { get; set; }
     }
 }
+
